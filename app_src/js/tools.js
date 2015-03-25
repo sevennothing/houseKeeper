d13d3344d42d62fd63bb4cfe13606fd943c19be0
caijun.Li <sevennothing@gmail.com>
2015-03-25 17:53:19 +0800

    commit app srouce(html5 code) with HBuilder

---
+++ app_src/js/tools.js
@@ -0,0 +1,8 @@
+function genCostIncomeId(key){
+	var id="TT201503251610SJ";
+	var now=new Date();
+	id = "CIT" + now.getFullYear().toString() + (now.getMonth() + 1).toString()
+		+ now.getDate().toString() + now.getHours().toString()
+		+ now.getMinutes().toString() + now.getSeconds().toString();
+	return id;
+}
