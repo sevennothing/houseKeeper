670d5c3b87e78fab916d772e0210df955c52d39c
caijun.Li <sevennothing@gmail.com>
2015-03-26 17:50:17 +0800

    storge

--- app_src/js/tools.js
+++ app_src/js/tools.js
@@ -1,8 +1,13 @@
-function genCostIncomeId(key){
+function genCostIncomeId(keys){
 	var id="TT201503251610SJ";
 	var now=new Date();
 	id = "CIT" + now.getFullYear().toString() + (now.getMonth() + 1).toString()
 		+ now.getDate().toString() + now.getHours().toString()
-		+ now.getMinutes().toString() + now.getSeconds().toString();
+		+ now.getMinutes().toString() + now.getSeconds().toString()
+		+ keys;
 	return id;
+}
+
+function checkVersion(){
+	
 }
